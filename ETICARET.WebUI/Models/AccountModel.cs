9474590ba16368c11007fa81d22feb6b1d82ddfe
tcanpolat <thsn_cnplt@windowslive.com>
2024-12-29 15:54:25 +0300

    web ui models,identity

---
+++ ETICARET.WebUI/Models/AccountModel.cs
@@ -0,0 +1,12 @@
+﻿using ETICARET.WebUI.Identity;
+
+namespace ETICARET.WebUI.Models
+{
+    public class AccountModel : ApplicationUser
+    {
+        public string FullName { get; set; }
+        public string Password { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+    }
+}
--- ETICARET.WebUI/Program.cs
+++ ETICARET.WebUI/Program.cs
@@ -23,6 +23,41 @@ builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
 var userManager = builder.Services.BuildServiceProvider().GetService<UserManager<ApplicationUser>>();
 var roleManager = builder.Services.BuildServiceProvider().GetService<RoleManager<IdentityRole>>();
 
+
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    options.Password.RequireNonAlphanumeric = true;
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequiredLength = 6;
+    
+    options.Lockout.MaxFailedAccessAttempts = 5;
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+    options.Lockout.AllowedForNewUsers = true;
+
+    options.User.RequireUniqueEmail = true;
+    options.SignIn.RequireConfirmedEmail = true;
+    options.SignIn.RequireConfirmedPhoneNumber = false;
+});
+
+
+// Cookie Options
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.LoginPath = "/account/login";
+    options.LogoutPath = "/account/logout";
+    options.AccessDeniedPath = "/account/accessdenied";
+    options.SlidingExpiration = true;
+    options.ExpireTimeSpan = TimeSpan.FromMinutes(60);
+    options.Cookie = new CookieBuilder
+    {
+        HttpOnly = true,
+        Name = "ETICARET.Security.Cookie",
+        SameSite = SameSiteMode.Strict
+    };
+});
+
 // Business and DataAccess
 builder.Services.AddScoped<IProductDal,EfCoreProductDal>();
 builder.Services.AddScoped<IProductService,ProductManager>();
@@ -43,22 +78,66 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+  
 }
 // Database Seed (Product,Category, Product Category)
 SeedDatabase.Seed();
 
-app.UseHttpsRedirection();
 app.UseStaticFiles();
-
+// middleware
+app.UseHttpsRedirection();
+app.UseAuthentication(); // kimlik doðrulama
+app.UseAuthorization(); // yetkilendirme
 app.UseRouting();
 
-app.UseAuthorization();
+// endpoints
+app.UseEndpoints( endpoints =>
+{
+    endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}");
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    endpoints.MapControllerRoute(
+        name: "products",
+        pattern: "products/{category}",
+        defaults: new { controller = "Shop", action = "List" }
+    );
+    endpoints.MapControllerRoute(
+        name: "adminProducts",
+        pattern: "admin/products",
+        defaults: new { controller = "Admin", action = "ProductList" }
+    );
+    endpoints.MapControllerRoute(
+        name: "adminProducts",
+        pattern: "admin/products/{id}",
+        defaults: new { controller = "Admin", action = "EditProduct" }
+    );
+    endpoints.MapControllerRoute(
+         name: "adminProducts",
+         pattern: "admin/category",
+         defaults: new { controller = "Admin", action = "CategoryList" }
+    );
+    endpoints.MapControllerRoute(
+        name: "adminProducts",
+        pattern: "admin/category/{id}",
+        defaults: new { controller = "Admin", action = "EditCategory" }
+    );
+    endpoints.MapControllerRoute(
+        name: "cart",
+        pattern: "cart",
+        defaults: new { controller = "Cart", action = "Index" }
+    );
+    endpoints.MapControllerRoute(
+        name: "checkout",
+        pattern: "checkout",
+        defaults: new { controller = "Cart", action = "Checkout" }
+    );
+    endpoints.MapControllerRoute(
+       name: "orders",
+       pattern: "orders",
+       defaults: new { controller = "Cart", action = "GetOrders" }
+   );
+
+}
+);
 
 
 SeedIdentity.Seed(userManager,roleManager,app.Configuration).Wait();
